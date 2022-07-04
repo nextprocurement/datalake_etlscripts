e4b8e112e74a0858b5bb055bb77c403f12becc16
Josep Ll. Gelpi <gelpi@ub.edu>
2022-07-04 12:39:55 +0200

    Added test notebook for crawling

--- read_parquet.py
+++ read_parquet.py
@@ -6,19 +6,16 @@ import numpy as np
 import sys
 import argparse
 
-
-
+codes_file, pkt_file = sys.argv[1:2]
 
 db_lnk = Mongo_db('localhost', 'nextprocurement', False, False)
 
-incoming_col, done_col = db_lnk.get_collections(['incoming']['done'])
+incoming_col, done_col = db_lnk.get_collections(['incoming','contratos'])
 
-pkt_file = '/home/gelpi/DEVEL/NEXTP/outsiders_2021.parquet'
 
 data_table = pd.read_parquet(pkt_file, use_nullable_dtypes=True)
 
-new_cols = pd.read_csv('columns.csv', sep='\t', index_col='ORIGINAL')
-
+new_cols = pd.read_csv(codes_file, sep='\t', index_col='ORIGINAL')
 
 id_num = 1
 for i in data_table.index:
@@ -26,8 +23,7 @@ for i in data_table.index:
     new_data = {'_id' : 'ntp{:s}'.format(str(id_num).zfill(8))}
     id_num += 1
     for k in data:
-        if isinstance(data[k], pd._libs.missing.NAType) or\
-            isinstance(data[k], pd._libs.tslibs.nattype.NaTType):
+        if pd.isna(data[k])
             data[k] = ''
         if isinstance(data[k], np.ndarray):
             data[k] = list(data[k])
