c447df8a731bcd9c6d90a895076562be60b18efd
Josep Ll. Gelpi <gelpi@ub.edu>
2022-07-06 04:58:35 +0200

    Refactored to define class

--- read_parquet.py
+++ read_parquet.py
@@ -6,27 +6,8 @@ import numpy as np
 import sys
 import argparse
 
-parser = argparse.ArgumentParser(description='Parse NextProcurement parquets')
-parser.add_argument('--drop', action='store_true')
-parser.add_argument('codes_file')
-parser.add_argument('pkt_file')
-args = parser.parse_args()
-
-db_lnk = Mongo_db('mdb-login.bsc.es', 'nextprocurement', False, True)
-
-incoming_col = db_lnk.get_collections(['incoming','contratos'])['incoming']
-
-data_table = pd.read_parquet(args.pkt_file, use_nullable_dtypes=True)
-
-new_cols = pd.read_csv(args.codes_file, sep='\t', index_col='ORIGINAL')
-if args.drop:
-    incoming_col.drop()
-    
-id_num = 1
-for i in data_table.index:
-    data = data_table.iloc[i].to_dict().copy()
-    new_data = {'_id' : 'ntp{:s}'.format(str(id_num).zfill(8))}
-    id_num += 1
+def parse_parquet(data):
+    new_data = {}
     for k in data:
         print(type(data[k]))
         if isinstance(data[k], np.ndarray):
@@ -34,14 +15,63 @@ for i in data_table.index:
         elif pd.isna(data[k]):
             data[k] = ''
         new_data[new_cols.loc[k]['DBFIELD']] = data[k]
-    try:
-        new_id = incoming_col.insert_one(new_data)
-    except Exception as e:
-        print(new_data)
-        for k in new_data:
-            print(k, new_data[k], type(new_data[k]))
-        print("ERROR",e)
-        sys.exit()
+    return new_data
 
+def parse_ntp_id(ntp_id):
+    return int(ntp_id.replace('ntp',''))
+class NtpEntry:
+    def __init__(ntp_order, data_row):
+        self.ntp_order = ntp_order
+        self.ntp_id = ''
+        self.data = parse_parquet(data_row)
+    
+    def get_ntp_id(self):
+        self.ntp_id = 'ntp{:s}'.format(str(self.ntp_order).zfill(8))
+
+    def order_from_id(self, id):
+        self.ntp_order = parse_ntp_id(self.ntp_id)
+
+    def commit_to_db(self, col):
+        self.data['_id'] = self.ntp_id
+        try:
+            new_id = col.insert_one(self.data)
+        except Exception as e:
+            print(self.data)
+            for k in self_data:
+                print(k, self_data[k], type(self_data[k]))
+            print("ERROR",e)
+            sys.exit()
+        return new_id
+
+parser = argparse.ArgumentParser(description='Parse NextProcurement parquets')
+parser.add_argument('--drop', action='store_true')
+parser.add_argument('codes_file')
+parser.add_argument('pkt_file')
+args = parser.parse_args()
+
+db_lnk = Mongo_db('mdb-login.bsc.es', 'nextprocurement', False, True)
+incoming_col = db_lnk.get_collections(['incoming','contratos'])['incoming']
+
+data_table = pd.read_parquet(args.pkt_file, use_nullable_dtypes=True)
+new_cols = pd.read_csv(args.codes_file, sep='\t', index_col='ORIGINAL')
+
+if args.drop:
+    incoming_col.drop()
+    id_num = 0
+else:
+    id_num = parse_ntp_id
+    (
+        list(
+            incoming_col.aggregate(
+                [{'$group':{'_id':'max_id', 'value':{'$max': '$_id'}}}]
+            )
+        )[0]['value']
+    )
+    
+for i in data_table.index:
+    data = data_table.iloc[i].to_dict().copy()
+    id_num += 1
+    new_data = NtpEntry(id_num, parse_parquet(data))
+    new_data.commit_to_db()
 
 
