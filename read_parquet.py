543bb435d1f0b35603023c96c83817e24c1bb011
Josep Ll. Gelpi <gelpi@ub.edu>
2022-07-06 02:34:36 +0200

    Added command line

--- read_parquet.py
+++ read_parquet.py
@@ -6,27 +6,33 @@ import numpy as np
 import sys
 import argparse
 
-codes_file, pkt_file = sys.argv[1:2]
+parser = argparse.ArgumentParser(description='Parse NextProcurement parquets')
+parser.add_argument('--drop', action='store_true')
+parser.add_argument('codes_file')
+parser.add_argument('pkt_file')
+args = parser.parse_args()
 
-db_lnk = Mongo_db('localhost', 'nextprocurement', False, False)
+db_lnk = Mongo_db('mdb-login.bsc.es', 'nextprocurement', False, True)
 
-incoming_col, done_col = db_lnk.get_collections(['incoming','contratos'])
+incoming_col = db_lnk.get_collections(['incoming','contratos'])['incoming']
 
+data_table = pd.read_parquet(args.pkt_file, use_nullable_dtypes=True)
 
-data_table = pd.read_parquet(pkt_file, use_nullable_dtypes=True)
-
-new_cols = pd.read_csv(codes_file, sep='\t', index_col='ORIGINAL')
-
+new_cols = pd.read_csv(args.codes_file, sep='\t', index_col='ORIGINAL')
+if args.drop:
+    incoming_col.drop()
+    
 id_num = 1
 for i in data_table.index:
     data = data_table.iloc[i].to_dict().copy()
     new_data = {'_id' : 'ntp{:s}'.format(str(id_num).zfill(8))}
     id_num += 1
     for k in data:
-        if pd.isna(data[k])
-            data[k] = ''
+        print(type(data[k]))
         if isinstance(data[k], np.ndarray):
             data[k] = list(data[k])
+        elif pd.isna(data[k]):
+            data[k] = ''
         new_data[new_cols.loc[k]['DBFIELD']] = data[k]
     try:
         new_id = incoming_col.insert_one(new_data)
