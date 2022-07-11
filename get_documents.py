53c1eb7b5d43d6c891581722aa9bb10317df165a
Josep Ll. Gelpi <gelpi@ub.edu>
2022-07-11 13:22:25 +0200

    Added support for external configuration yml file

--- get_documents.py
+++ get_documents.py
@@ -2,110 +2,54 @@
 # coding: utf-8
 from mmb_data.mongo_db_connect import Mongo_db
 import sys
+from swift_secrets import MONGODB_HOST
 import swiftclient as sw
-import swift_secrets as ss
-# import re
 import argparse
-# import json
+from yaml import load, CLoader
 import requests
 import ntp_entry as ntp
 
 tmpdir = "/tmp"
-# def parse_ntp_id(ntp_id):
-#     return int(ntp_id.replace('ntp',''))
-# def check_ntp_id(ntp_id):
-#     return re.match(r'^ntp[0-9]{8}', ntp_id)
-
-# class NtpEntry:
-#     def __init__(self):
-#         self.ntp_order = 0
-#         self.ntp_id = ''
-#         self.data = {}
-
-#     def load_data(self, ntp_order, data):
-#         self.ntp_order = ntp_order
-#         self.set_ntp_id()
-#         self.data = data
-
-#         self.data['_id'] = self.ntp_id
-#     def set_ntp_id(self):
-#         self.ntp_id = 'ntp{:s}'.format(str(self.ntp_order).zfill(8))
-
-#     def order_from_id(self, id):
-#         self.ntp_order = parse_ntp_id(self.ntp_id)
-
-#     def commit_to_db(self, col):
-#         try:
-#             new_id = col.insert_one(self.data)
-#         except Exception as e:
-#             print(self.data)
-#             for k in self.data:
-#                 print(k, self.data[k], type(self.data[k]))
-#             print("ERROR",e)
-#             sys.exit()
-#         return new_id
-
-#     def load_from_db(self, col_id,  ntp_id):
-#         try:
-#             self.data = col_id.find_one({'_id': ntp_id})
-#             self.ntp_id = ntp_id
-#             self.ntp_order = parse_ntp_id(ntp_id)
-#         except Exception as e:
-#             print(e)
-#             sys.exit()
-
-#     def extract_url(self):
-#         urls = {}
-#         for k in self.data:
-#             if isinstance(self.data[k], str) and self.data[k].startswith('http'):
-#                 urls[k] = self.data[k]
-#         return urls
-
-#     def download_document(
-#             self, 
-#             field,
-#             towhere='disk', 
-#             tmpdir='/tmp', 
-#             grid_fs_col='',
-#             update=False
-#             ):
-#         file_name = f'{self.ntp_id}_{field}.pdf'
-#         if update or not self.check_file_exists(
-#                 file_name, 
-#                 towhere=towhere
-#                 tmpdir=tmpdir, 
-#                 grid_fs_col=grid_fs_col''
-#             )
-
-#             url = self.data[field]
-#             r = requests.get(url, stream=True)
-#             if r.status_code == 200:
-#                 if 'Content-type' not in r.headers or r.headers['Content-type'].startswith('text/html') :
-#                     return False
-#                 if towhere == 'disc':
-#                     with open(f"{tmpdir}/{file_name}", 'bw') as output_file:
-#                         output_file.write(r.content)
-#                 elif towhere == 'gridfs':
-#                     grid_fs_col.put(r.content, filename=file_name)
-#                 elif towhere == 'swift':
-#                     pass
-#             return True
-#         return False
-        
-#         def check_file_exists(self, file_name, towhere, tmpdir, grid_fs_col):
-#             return False
 
 def main():
+    # usage: get_documents.py [-h] [--update] [--ini INI] [--fin FIN] [--id ID]
+    #                         [--where {disc,gridfs,swift}]
+
+    # Download documents
+
+    # optional arguments:
+    #   -h, --help            show this help message and exit
+    #   --update              Add new files only
+    #   --ini INI             Initial document range
+    #   --fin FIN             Final document range
+    #   --id ID               Selected document id
+    #   --where {disc,gridfs,swift}
+    #                         Selected storage (disk|gridfs|swift)    
+    #   --config              Configuration file
+
     parser = argparse.ArgumentParser(description='Download documents')
-    parser.add_argument('--update', action='store_true')
-    parser.add_argument('--ini', action='store')
-    parser.add_argument('--fin', action='store')
-    parser.add_argument('--where', action='store', default='disc', choices=['disc', 'gridfs', 'swift'])
+    parser.add_argument('--update', action='store_true', help="Add new files only")
+    parser.add_argument('--ini', action='store', help="Initial document range")
+    parser.add_argument('--fin', action='store', help="Final document range")
+    parser.add_argument('--id', action='store', help="Selected document id")
+    parser.add_argument('--where', action='store', default='disc', choices=['disc', 'gridfs', 'swift'], help="Selected storage (disk|gridfs|swift)")
+    parser.add_argument('--config'  action='store' default='secrets.yml', help="Configuration file (default;secrets.yml)")
     args = parser.parse_args()
 
+    # Config file
+    with open(args.config)  as config_file:
+        config = load(config_file, Loader=CLoader)
+    
     print("Connecting to MongoDB")
-    db_lnk = Mongo_db('mdb-login.bsc.es', 'nextprocurement', False, True)
+    db_lnk = Mongo_db(
+        config['MONGODB_HOST'],
+        'nextprocurement', 
+        False, 
+        config['MONGODB_AUTH']
+        credentials=config['MONGODB_CREDENTIALS']
+    )
     incoming_col = db_lnk.get_collections(['incoming','contratos'])['incoming']
+
     print("Connecting to storage...")
     if args.where == 'disc':
         storage = ntp.NtpStorage(type='disc', data_dir='/tmp')
@@ -113,13 +57,13 @@ def main():
         storage = ntp.NtpStorage(type='gridfs', gridfs_obj=db_lnk.get_gfs('downloadedDocuments'))
     elif args.where == 'swift':
         swift_conn = sw.Connection(
-            authurl=ss.OS_AUTH_URL, 
+            authurl=config['OS_AUTH_URL'], 
             auth_version=3,
             os_options = {
-                'auth_type': ss.OS_AUTH_TYPE,
-                'region_name': ss.OS_REGION_NAME,
-                'application_credential_id': ss.OS_APPLICATION_CREDENTIAL_ID,
-                'application_credential_secret': ss.OS_APPLICATION_CREDENTIAL_SECRET,
+                'auth_type': config['OS_AUTH_TYPE'],
+                'region_name': config['OS_REGION_NAME'],
+                'application_credential_id': config['OS_APPLICATION_CREDENTIAL_ID'],
+                'application_credential_secret': config['OS_APPLICATION_CREDENTIAL_SECRET'],
                 'service_project_name': 'bsc22NextProcurement'
             }
         )
@@ -129,28 +73,35 @@ def main():
             swift_container='nextp-data',
             swift_prefix='data'
         )
+
     print("Getting ids...")
-    # Getting ids
-    query = [{}]
-    if args.ini is not None:
-        if ntp.check_ntp_id(args.ini):
-            query.append({'_id':{'$gte': args.ini}})
-        else:
-            print(f'--ini {args.ini} argument is not a valid ntp id')
-            sys.exit()
-    if args.fin is not None:
-        if ntp.check_ntp_id(args.fin):
-            query.append({'_id':{'$lte': args.fin}})
+    if args.id:
+        if ntp.check_ntp_id(args.id):
+            query = {'_id': args.id}
         else:
             print(f'--fin {args.fin} argument is not a valid ntp id')
             sys.exit()
+    else:
+        query = [{}]
+        if args.ini is not None:
+            if ntp.check_ntp_id(args.ini):
+                query.append({'_id':{'$gte': args.ini}})
+            else:
+                print(f'--ini {args.ini} argument is not a valid ntp id')
+                sys.exit()
+        if args.fin is not None:
+            if ntp.check_ntp_id(args.fin):
+                query.append({'_id':{'$lte': args.fin}})
+            else:
+                print(f'--fin {args.fin} argument is not a valid ntp id')
+                sys.exit()
 
-    id_list = list(incoming_col.find({'$and':query}, {'_id':1}))
-    for id in id_list:
+    for id in list(incoming_col.find({'$and':query}, {'_id':1})):
         ntp_id = id['_id']
         doc = ntp.NtpEntry()
         doc.load_from_db(incoming_col, ntp_id)
         #print(vars(doc))
+
         for url_field in doc.extract_urls():
             print(url_field, doc.data[url_field])
             if doc.download_document(url_field, storage=storage):
--- read_parquet.py
+++ read_parquet.py
@@ -4,57 +4,48 @@ import pandas as pd
 # import numpy as np
 import sys
 import argparse
+from yaml import load, CLoader
+
+
 
 import ntp_entry as ntp
 from mmb_data.mongo_db_connect import Mongo_db
 
-# def parse_parquet(pd_data_row, new_cols):
-#     new_data = {}
-#     print(pd_data_row)
-#     for k in pd_data_row:
-#         print(k)
-#         if isinstance(pd_data_row[k], np.ndarray):
-#             pd_data_row[k] = list(pd_data_row[k])
-#         elif pd.isna(pd_data_row[k]):
-#             pd_data_row[k] = ''
-#         new_data[new_cols.loc[k]['DBFIELD']] = pd_data_row[k]
-#     return new_data
-
-# def parse_ntp_id(ntp_id):
-#     return int(ntp_id.replace('ntp',''))
-# class NtpEntry:
-#     def __init__(self, ntp_order, data):
-#         self.ntp_order = ntp_order
-#         self.set_ntp_id()
-#         self.data = data
-#         self.data['_id'] = self.ntp_id
-        
-#     def set_ntp_id(self):
-#         self.ntp_id = 'ntp{:s}'.format(str(self.ntp_order).zfill(8))
-
-
-#     def order_from_id(self, id):
-#         self.ntp_order = parse_ntp_id(self.ntp_id)
-
-#     def commit_to_db(self, col):
-#         try:
-#             new_id = col.insert_one(self.data)
-#         except Exception as e:
-#             print(self.data)
-#             for k in self.data:
-#                 print(k, self.data[k], type(self.data[k]))
-#             print("ERROR",e)
-#             sys.exit()
-#         return new_id
+MONGODB_HOST = 'mdb-login.bsc.es'
+MONGODB_AUTH = True
 
 def main():
+    # usage: read_parquet.py [-h] [--drop] codes_file pkt_file
+
+    # Parse NextProcurement parquets
+
+    # positional arguments:
+    #   codes_file  Columns sanitized names
+    #   pkt_file    Parquet file
+
+    # optional arguments:
+    #   -h, --help  show this help message and exit
+    #   --drop      Clean MongoDB collection
+    #   --config    Configuration file (default:secrets.yml)    
+
     parser = argparse.ArgumentParser(description='Parse NextProcurement parquets')
-    parser.add_argument('--drop', action='store_true')
-    parser.add_argument('codes_file')
-    parser.add_argument('pkt_file')
+    parser.add_argument('--drop', action='store_true', help="Clean MongoDB collection")
+    parser.add_argument('--config', action='store', help="Configuration file", default="secrets.yml")
+    parser.add_argument('codes_file', help="Columns sanitized names")
+    parser.add_argument('pkt_file', help="Parquet file")
     args = parser.parse_args()
 
-    db_lnk = Mongo_db('mdb-login.bsc.es', 'nextprocurement', False, True)
+    # Config file
+    with open(args.config)  as config_file:
+        config = load(config_file, Loader=CLoader)
+
+    db_lnk = Mongo_db(
+        config['MONGODB_HOST'], 
+        'nextprocurement', 
+        False, 
+        config['MONGODB_AUTH'], 
+        credentials=config['MONGODB_CREDENTIALS']
+    )
     incoming_col = db_lnk.get_collections(['incoming','contratos'])['incoming']
 
     data_table = pd.read_parquet(args.pkt_file, use_nullable_dtypes=True)
@@ -64,10 +55,10 @@ def main():
         incoming_col.drop()
         id_num = 0
     else:
-        max_id = incoming_col.aggregate(
+        max_id_c = incoming_col.aggregate(
                     [{'$group':{'_id':'max_id', 'value':{'$max': '$_id'}}}]
                 )
-        id_num = ntp.parse_ntp_id(list(max_id)[0]['value'])
+        id_num = ntp.parse_ntp_id(list(max_id_c)[0]['value'])
     
     print(f"Last reference found {id_num}")
 
