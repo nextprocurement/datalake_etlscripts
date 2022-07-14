82490c3cba82b375a9db5b0c3e9eaa9e7e5e1b13
Josep Ll. Gelpi <gelpi@ub.edu>
2022-07-14 15:05:28 +0200

    Classes split in separated files

--- ntp_entry.py
+++ ntp_entry.py
@@ -1,63 +1,44 @@
-
+''' Classes NtoStorage and NtpEntry '''
 import pandas as pd
 import requests
 import numpy as np
 import sys
 import re
+import os.path
+from os.path import join as opj
 import logging
 import argparse
 from mmb_data.mongo_db_connect import Mongo_db
 
 def parse_ntp_id(ntp_id):
+    ''' Get document order from ntp_id
+        Parameters:
+            ntp_id (str): ntp id as ntp[0-9]{8}
+    '''
     return int(ntp_id.replace('ntp',''))
 
 def check_ntp_id(ntp_id):
+    ''' Check ntp id syntax
+        Parameters:
+            ntp_if (str)
+    '''
     return re.match(r'^ntp[0-9]{8}', ntp_id)
 
 def parse_parquet(pd_data_row, new_cols):
+    ''' Parse data Pandas' data row read from a parquet file
+        Parameters:
+            pd_data_row (Pandas' data row): Single entry from pandas dataframe
+            new_cols (dict): Dictionary with translated columns names
+    '''
     new_data = {}
-    for k in pd_data_row:
-        if isinstance(pd_data_row[k], np.ndarray):
-            pd_data_row[k] = list(pd_data_row[k])
-        elif pd.isna(pd_data_row[k]):
-            pd_data_row[k] = ''
-        new_data[new_cols.loc[k]['DBFIELD']] = pd_data_row[k]
+    for col in pd_data_row:
+        if isinstance(pd_data_row[col], np.ndarray):
+            pd_data_row[col] = list(pd_data_row[col])
+        elif pd.isna(pd_data_row[col]):
+            pd_data_row[col] = ''
+        new_data[new_cols.loc[col]['DBFIELD']] = pd_data_row[col]
     return new_data
 
-class NtpStorage:
-    def __init__(self, type='disc', **kwargs):
-        self.type = type
-        if type == 'disc':
-            self.data_dir = kwargs['data_dir']
-        elif type == 'gridfs':
-            self.gridfs = kwargs['gridfs_obj']
-        elif type == 'swift':
-            self.swift_connection = kwargs['swift_connection']
-            self.swift_container = kwargs['swift_container']
-            self.swift_prefix = kwargs['swift_prefix']
-    
-    def file_store(self, file_name, contents):   
-        if self.type == 'disc':
-            with open(f"{self.data_dir}/{file_name}", 'bw') as output_file:
-                    output_file.write(contents)
-        elif self.type == 'gridfs':
-            self.gridfs.put(contents, filename=file_name)
-        elif self.type == 'swift':
-            self.swift_connection.put_object(
-                self.swift_container,
-                f"{self.swift_prefix}/{file_name}",
-                contents=contents
-            )
-    
-    def file_exists(self, file_name):
-        #TODO
-        if self.type == 'disc':
-            return False
-        elif self.type == 'gridfs':
-            return False
-        elif self.type == 'swift':
-            return False
-    
 class NtpEntry:
     def __init__(self):
         self.ntp_order = 0
@@ -103,20 +84,35 @@ class NtpEntry:
                 urls[k] = self.data[k]
         return urls
 
-    def download_document(
-            self, 
+    def get_file_name(self, field):
+        return f'{self.ntp_id}_{field}.pdf'
+
+    def store_document(
+            self,
             field,
             storage=None,
-            update=False
+            replace=False
             ):
-        file_name = f'{self.ntp_id}_{field}.pdf'
-        if update or not storage.file_exists(file_name):  
+        file_name = self.get_file_name(field)
+        if replace or not storage.file_exists(file_name):
             url = self.data[field]
             r = requests.get(url, stream=True)
+            logging.debug(f"Headers: {r.headers}")
             if r.status_code == 200:
-                if 'Content-type' not in r.headers or r.headers['Content-type'].startswith('text/html') :
-                    return False
-                storage.file_store(file_name, r.content)
-            return True
-        return False
+                #TODO check headers for more options
+                not_valid_document = check_document(r.headers) 
+                if not_valid_document:
+                    return not_valid_document
+                else:
+                    storage.file_store(file_name, r.content)
+                return 0
+        return 1
+
+def check_document(headers):
+    #TODO check other options
+    if 'Content-type' not in headers or headers['Content-type'].startswith('text/html'):
+        return 2
+    else:
+        return 0
+              
     
---
+++ ntp_storage.py
@@ -0,0 +1,72 @@
+''' Classes NtoStorage '''
+import sys
+import os.path
+from os.path import join as opj
+import swiftclient as sw
+
+# from mmb_data.mongo_db_connect import Mongo_db
+
+class NtpStorage:
+    ''' Abstract class to manage alternative storages'''
+    def __init__(self, type_store):
+        self.type = type_store
+
+class NtpStorageDisk (NtpStorage):
+    ''' Class to manage disk storage'''
+    def __init__(self, type_store='disk', data_dir=''):
+        super().__init__(type_store=type_store)
+        self.data_dir = data_dir
+
+    def file_store(self, file_name, contents):
+
+        with open(opj(self.data_dir, file_name), 'bw') as output_file:
+            output_file.write(contents)
+
+    def file_exists(self, file_name):
+        return os.path.exists(opj(self.data_dir, file_name))
+
+class NtpStorageGridFs (NtpStorage):
+    '''Class to manage GridFS storage'''
+    def __init__(self, type_store='gridfs', gridfs_obj=None):
+        super().__init__(type_store=type_store)
+        self.gridfs = gridfs_obj
+
+    def file_store(self, file_name, contents):
+        #removing previous version if exists
+        if self.file_exists(file_name):
+            file_id = self.gridfs.find_one({'filename':file_name})._id
+            self.gridfs.delete(file_id)
+        self.gridfs.put(contents, filename=file_name)
+
+    def file_exists(self, file_name):
+        return self.gridfs.exists(filename=file_name)
+
+class NtpStorageSwift (NtpStorage):
+    '''Class to manage Swift storage'''
+    def __init__(self, type_store='swift', **kwargs):
+        super().__init__(type_store=type_store)
+        self.connection = kwargs['swift_connection']
+        self.container = kwargs['swift_container']
+        self.data_prefix = kwargs['swift_prefix']
+
+    def file_store(self, file_name, contents):
+        self.connection.put_object(
+            self.container,
+            opj(self.data_prefix, file_name),
+            contents=contents
+        )
+
+    def file_exists(self, file_name):
+        try:
+            resp_headers = self.connection.head_object(
+                self.container, 
+                opj(self.data_prefix, file_name)
+            )
+            return True
+        except sw.ClientException as e:            
+            print(e.http_status)
+            if e.http_status == 404:
+                return False
+            else:
+                print("Error checking swift storage")            
+                sys.exit()
