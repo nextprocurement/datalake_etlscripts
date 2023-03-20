f437f5f02cf810a6beee6f84f73a6eeb1defdaab
Josep Ll. Gelpi <gelpi@ub.edu>
2023-03-20 17:36:31 +0100

    Initial commit

---
+++ process_place.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+''' Script to load PDF documents onto data_lake, takes input data from MongoDB
+    usage: process_place.py [-h] [--ini INI] [--fin FIN] [--id ID]
+'''
+import sys
+import argparse
+import logging
+import os
+import time
+from yaml import load, CLoader
+import ntp_entry as ntp
+import ntp_storage as ntpst
+from mmb_data.mongo_db_connect import Mongo_db
+
+def main():
+    ''' Main '''
+
+    parser = argparse.ArgumentParser(description='Download documents')
+    parser.add_argument('--replace', action='store_true', help='Replace existing files')
+    parser.add_argument('--ini', action='store', help='Initial document range')
+    parser.add_argument('--fin', action='store', help='Final document range')
+    parser.add_argument('--id', action='store', help='Selected document id')
+    parser.add_argument('--config', action='store', default='secrets.yml', help='Configuration file (default;secrets.yml)')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Extra progress information')
+    parser.add_argument('--debug',action='store_true', help='Extra debug information')
+
+    args = parser.parse_args()
+    # Setup logging
+    logging.basicConfig(stream=sys.stdout, format='[%(asctime)s] %(levelname)s %(message)s', datefmt='%Y-%m-%d|%H:%M:%S')
+    if args.debug:
+        logging.getLogger().setLevel(10)
+    else:
+        logging.getLogger().setLevel(20)
+
+    # Config file
+    with open(args.config, 'r')  as config_file:
+        config = load(config_file, Loader=CLoader)
+
+    logging.info("Connecting to MongoDB")
+
+    db_lnk = Mongo_db(
+        config['MONGODB_HOST'],
+        'nextprocurement',
+        False,
+        config['MONGODB_AUTH'],
+        credentials=config['MONGODB_CREDENTIALS'],
+        connect_db=True
+    )
+    incoming_col = db_lnk.db.get_collection('place')
+    curated_col = db_lnk.db.get_collection('place_curated')
+
+    if args.verbose:
+        logging.info("Getting ids...")
+
+    for ntp_id in (args.id, args.ini, args.fin):
+        if ntp_id is not None and not ntp.check_ntp_id(ntp_id):
+            logging.error(f'{ntp_id} is not a valid ntp id')
+            sys.exit()
+
+    if args.id is not None:
+        query = {'_id': args.id}
+    else:
+        query = [{}]
+        if args.ini is not None:
+            query.append({'_id':{'$gte': args.ini}})
+        if args.fin is not None:
+            query.append({'_id':{'$lte': args.fin}})
+        query = {'$and': query}
+
+    num_ids = 0
+    num_place_ids = 0
+    LICS = {}
+    for doc in list(incoming_col.find(query, {'_id':1, 'id':1})):
+        ntp_id = doc['_id']
+        place_id = os.path.basename(doc['id'])
+        if place_id not in LICS:
+            LICS[place_id] = [ntp_id]
+            num_place_ids += 1
+        else:
+            LICS[place_id].append(ntp_id)
+        num_ids += 1
+        ntp_doc = ntp.NtpEntry()
+        ntp_doc.load_from_db(incoming_col, ntp_id)
+    logging.info(f"{num_ids} available, grouped on {num_place_ids} licitations")
+
+if __name__ == "__main__":
+    main()
