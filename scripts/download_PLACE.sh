e33aad3c8cdc233b0c8cd217d98bb96abfd7dd5e
Josep Ll. Gelpi <gelpi@ub.edu>
2023-12-04 14:25:21 +0000

    Added download only script

---
+++ scripts/download_PLACE.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+export BASEDIR=/home/ubuntu/ETL
+export DATADIR=/data/incoming/PLACE_NEW
+export EXEDIR=$BASEDIR/etlscripts
+#Agregados
+export BASEURL=https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_1044/PlataformasAgregadasSinMenores_
+cd $DATADIR/outsiders
+wget -nv -N --no-check-certificate ${BASEURL}$1.zip
+#Menores
+export BASEURL=https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_1143/contratosMenoresPerfilesContratantes_
+cd $DATADIR/minors
+wget -nv -N --no-check-certificate ${BASEURL}$1.zip
+#perfiles
+export BASEURL=https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3_
+cd $DATADIR/insiders
+wget -nv -N --no-check-certificate ${BASEURL}$1.zip
