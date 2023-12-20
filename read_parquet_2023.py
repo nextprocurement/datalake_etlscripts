d71f212dfcfbf9482fef6c2082dc5a56b4fa632a
Josep Ll. Gelpi <gelpi@ub.edu>
2023-12-20 10:00:00 +0000

    Reorganized files

--- read_parquet_2023.py
+++ read_parquet_2023.py
@@ -66,8 +66,7 @@ def main():
         connect_db=True
     )
 
-    #incoming_col = db_lnk.db.get_collection(config[f'{args.group}_col_prefix'])
-    incoming_col = db_lnk.db.get_collection('place_test') # testing only
+    incoming_col = db_lnk.db.get_collection(config[f'{args.group}_col_prefix'])
 
     data_table = pd.read_parquet(args.pkt_file, use_nullable_dtypes=True)
     new_cols = pd.read_csv(args.codes_file, sep='\t', index_col='ORIGINAL')
