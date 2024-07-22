39909b0825be1cec3ff70308bddd6041344c3675
Josep Ll. Gelpi <gelpi@ub.edu>
2024-07-22 19:12:29 +0200

    Added multiple files input

--- parse_bsc_companies.py
+++ parse_bsc_companies.py
@@ -32,7 +32,8 @@ def main():
     parser.add_argument('-v','--verbose', action='store_true', help="Add Extra information")
     parser.add_argument('--dry_run', action='store_true', help="Do not alter DB, just check")
 
-    parser.add_argument('json_file', help="INput file")
+    parser.add_argument('json_files', help="Input file", nargs='+')
+
     args = parser.parse_args()
 
     logging.basicConfig(stream=sys.stdout, format='[%(asctime)s] %(levelname)s %(message)s', datefmt='%Y-%m-%d|%H:%M:%S')
@@ -61,50 +62,55 @@ def main():
     place_cols = [db_lnk.db.get_collection(config["outsiders_col_prefix"]), db_lnk.db.get_collection(config["minors_col_prefix"])]
     logging.debug(f"Place collections: {place_cols}")
 
-    processed_docs = {}
 
-    with open(args.json_file) as json_file:
-        for line in json_file:
-            data = json.loads(line)
-            if all(x not in data for x in ['SINGLE_COMPANY', 'UTE']):
-                logging.warning(f"Document {data['doc_name']} does not have companies")
-                continue
+    for file in args.json_files:
+        logging.info(f"Processing {file}")
 
-            if data['procurement_id'] not in processed_docs:
-                logging.debug(f"Processing {data['procurement_id']}")
-                col = place_cols[nu.get_group(data['procurement_id'])]
-                ref_doc = ntp.NtpEntry()
-                ref_doc.load_from_db(col, data['procurement_id'])
-                if ref_doc.is_obsolete():
-                    active_doc_id = nu.get_active_version(ref_doc.data['id'], col)
-                    logging.warning(f"Document {data['procurement_id']} is obsolete, active version is {active_doc_id}")
-                    ref_doc.load_from_db(col, active_doc_id)
-                    if not ref_doc:
-                        logging.error(f"Active version {active_doc_id} not found")
-                        continue
+        processed_docs = {}
+
+        with open(file) as json_file:
+            for line in json_file:
+                data = json.loads(line)
+                if all(x not in data for x in ['SINGLE_COMPANY', 'UTE']):
+                    logging.warning(f"Document {data['doc_name']} does not have companies")
+                    continue
+
+                if data['procurement_id'] not in processed_docs:
+                    logging.debug(f"Processing {data['procurement_id']}")
+                    col = place_cols[nu.get_group(data['procurement_id'])]
+                    ref_doc = ntp.NtpEntry()
+                    ref_doc.load_from_db(col, data['procurement_id'])
+                    if ref_doc.is_obsolete():
+                        active_doc_id = nu.get_active_version(ref_doc.data['id'], col)
+                        logging.warning(f"Document {data['procurement_id']} is obsolete, active version is {active_doc_id}")
+                        ref_doc.load_from_db(col, active_doc_id)
+                        if not ref_doc:
+                            logging.error(f"Active version {active_doc_id} not found")
+                            continue
+                    else:
+                        logging.info(f"Document {data['procurement_id']} is active")
+                    processed_docs[data['procurement_id']] = ref_doc.ntp_id
                 else:
-                    logging.info(f"Document {data['procurement_id']} is active")
-                processed_docs[data['procurement_id']] = ref_doc.ntp_id
-            else:
-                logging.debug(f"Document {data['procurement_id']} already found as {processed_docs[data['procurement_id']]}")
-                ref_doc = ntp.NtpEntry()
-                ref_doc.load_from_db(col, processed_docs[data['procurement_id']])
+                    logging.debug(f"Document {data['procurement_id']} already found as {processed_docs[data['procurement_id']]}")
+                    ref_doc = ntp.NtpEntry()
+                    ref_doc.load_from_db(col, processed_docs[data['procurement_id']])
 
-            if 'empresas_en_docs' not in ref_doc.data:
-                ref_doc.data['empresas_en_docs'] = {}
+                if 'empresas_en_docs' not in ref_doc.data:
+                    ref_doc.data['empresas_en_docs'] = {}
 
-            ref_doc.data['empresas_en_docs'][data['doc_name']] = {}
-            for group in ('SINGLE_COMPANY', 'UTE'):
-                if group in data and data[group]:
-                    ref_doc.data['empresas_en_docs'][data['doc_name']][group] = data[group]
+                ref_doc.data['empresas_en_docs'][data['doc_name']] = {}
+                for group in ('SINGLE_COMPANY', 'UTE'):
+                    if group in data and data[group]:
+                        ref_doc.data['empresas_en_docs'][data['doc_name']][group] = data[group]
 
-            logging.debug(f"Document {ref_doc.ntp_id} to update ")
-            logging.debug(ref_doc.data['empresas_en_docs'])
-            if not args.dry_run:
-                ref_doc.commit_to_db(col)
-            else:
-                logging.info(f"Document {ref_doc.ntp_id} not saved (--dry_run)")
-
+                logging.debug(f"Document {ref_doc.ntp_id} to update ")
+                logging.debug(ref_doc.data['empresas_en_docs'])
+                if not args.dry_run:
+                    ref_doc.commit_to_db(col)
+                else:
+                    logging.info(f"Document {ref_doc.ntp_id} not saved (--dry_run)")
+        logging.info(f"Processed {len(processed_docs)} documents")
+    logging.info("Done")
 
 
 if __name__ == "__main__":
