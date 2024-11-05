28f0299a8de16669bde438fba8098a9fe3a3aed3
Josep Ll. Gelpi <gelpi@ub.edu>
2024-11-05 16:59:00 +0000

    Added simpler version

--- checking/clean_documents.py
+++ checking/clean_documents.py
@@ -22,8 +22,8 @@ def main():
     parser.add_argument('--config', action='store', default='secrets.yml', help='Configuration file (default;secrets.yml)')
     parser.add_argument('-v', '--verbose', action='store_true', help='Extra progress information')
     parser.add_argument('--debug',action='store_true', help='Extra debug information')
-    parser.add_argument('--output',action='store', help='Output filename index')
-
+    parser.add_argument('--drop', action='store_true', help='drop added info')
+    
     args = parser.parse_args()
     # Setup logging
     logging.basicConfig(stream=sys.stdout, format='[%(asctime)s] %(levelname)s %(message)s', datefmt='%Y-%m-%d|%H:%M:%S')
@@ -56,49 +56,45 @@ def main():
     backup_storage = ntpst.NtpStorageGridFs(gridfs_obj=db_lnk.get_gfs(config['documents_backup_col']))
     files_col = db_lnk.db.get_collection(config['documents_col'] + '.files')
     
-    processed_docs = {}
-    docs_id_index = {}
-    new_files = set()
+    if args.drop:
+        query = {}
+        logging.info("Dropping place_filenames")
+        files_col.update_many({}, {'$unset': {'place_filename': 1, 'duplicate' : 1}})
+    else:
+        query = {'place_filename': {'$exists': False}}        
+        logging.info("Adding non exisiting place_filenames")
+      
 
-    for file in files_col.find():
-        id, field = file['filename'].split('_', 1)
-        if id not in processed_docs:
-            logging.debug(f"Processing {id}")
-            col = place_cols[nu.get_group(id)]
-            ref_doc = ntp.NtpEntry()
-            ref_doc.load_from_db(col, id)
-            if ref_doc.is_obsolete():
-                active_doc_id = nu.get_active_version(ref_doc.data['id'], col)
-                logging.warning(f"Document {id} is obsolete, active version is {active_doc_id}")
-                ref_doc.load_from_db(col, active_doc_id)
-                if not ref_doc:
-                    logging.error(f"Active version {active_doc_id} not found")
-                    continue
-            else:
-                logging.info(f"Document {id} is active")
-            processed_docs[id] = ref_doc.ntp_id
+
+    for file in files_col.find(query, no_cursor_timeout=True):
+        if not args.drop and 'place_filename' in file and file['place_filename']:
+            logging.warning(f"File {file['filename']} already processed")
+            continue
+        ntp_id, field = file['filename'].split('_', 1)
+        logging.debug(f"Processing {ntp_id}")   
+        col = place_cols[nu.get_group(ntp_id)]
+        ref_doc = ntp.NtpEntry()
+        if not ref_doc.load_from_db(col, ntp_id):
+            logging.error(f"Document {ntp_id} not found")
+            continue
+        if not ref_doc.data['id']:
+            logging.error(f"Document {ntp_id} has no place_id")
+            continue
+        place_filename = f"PL{format(int(ref_doc.data['id'].split('/')[-1]), '08d')}_{field}"
+        if file['md5']:
+            logging.debug(f"Updating {file['filename']} to {place_filename}")   
+            files_col.update_one({'_id': file['_id']}, {'$set': {'place_filename': place_filename}})
+            for duplicate in files_col.find({'md5': file['md5']}):
+                if duplicate['filename'] == file['filename']:
+                    continue    
+                logging.debug(f"Found duplicated document {duplicate['filename']}, marking as duplicate")
+                files_col.update_one(
+                    {'_id': duplicate['_id']}, 
+                    {'$set': {'place_filename': place_filename, 'duplicate':True}}
+                )
         else:
-            logging.debug(f"Document {id} already found as {processed_docs[id]}")
-            ref_doc = ntp.NtpEntry()
-            ref_doc.load_from_db(col, processed_docs[id])
-    
-        if ref_doc.data['id'] in new_ids:
-            logging.warning(f"Found Duplicated file at {ref_doc.data['id']}")
-        versions = set([x['_id'] for x in nu.get_versions(ref_doc.data['id'], col)])
+            logging.warning(f"File {file['filename']} has no md5")
 
-        new_id = f"PL{format(int(ref_doc.data['id'].split('/')[-1]), '08d')}"
-        new_filename = f"{new_id}_{field}"
-        if new_filename in new_files:
-            logging.warning(f"Found duplicated {new_filename}")
-        new_files.add(new_filename)
-
-        print(json.dumps({'id':new_id, 'versions':sorted(list(versions))}))
-        docs_id_index[file['filename']] = new_filename
-
-
-    with open('args.output', "w") as output_file:
-        for old_filename, new_filename in docs_id_index.items():
-            print(f"{old_filename} {docs_id_index[new_filename]}", file=output_file)
 
 
 if __name__ == "__main__":
