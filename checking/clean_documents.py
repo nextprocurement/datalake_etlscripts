9343d967ef0336985a6517eb203c1bb025191bea
Josep Ll. Gelpi <gelpi@ub.edu>
2024-11-22 12:02:51 +0000

    Improved treatment of duplicates

--- checking/clean_documents.py
+++ checking/clean_documents.py
@@ -13,7 +13,14 @@ from datetime import datetime
 from yaml import load, CLoader
 from nextplib import ntp_entry as ntp, ntp_storage as ntpst, ntp_constants as cts, ntp_utils as nu
 from mmb_data.mongo_db_connect import Mongo_db
+from mmb_data.mongo_db_bulk_write import MongoDBBulkWrite, CTS
 
+def is_valid_field(valid_fields, field):
+    field = field.split('.')[0].split(':')[0]
+    print(field)
+    if field in valid_fields:
+        return field
+    return False
 
 def main():
     ''' Main '''
@@ -23,7 +30,7 @@ def main():
     parser.add_argument('-v', '--verbose', action='store_true', help='Extra progress information')
     parser.add_argument('--debug',action='store_true', help='Extra debug information')
     parser.add_argument('--drop', action='store_true', help='drop added info')
-    
+
     args = parser.parse_args()
     # Setup logging
     logging.basicConfig(stream=sys.stdout, format='[%(asctime)s] %(levelname)s %(message)s', datefmt='%Y-%m-%d|%H:%M:%S')
@@ -46,33 +53,38 @@ def main():
         credentials=config['MONGODB_CREDENTIALS'],
         connect_db=True
     )
-
-
-    place_cols = [db_lnk.db.get_collection(config["outsiders_col_prefix"]), db_lnk.db.get_collection(config["minors_col_prefix"])]
+    mdb_session = db_lnk.client.start_session()
+    logging.info("Starting mongodb session")
+    place_cols = [
+        db_lnk.db.get_collection(config["outsiders_col_prefix"]), 
+        db_lnk.db.get_collection(config["minors_col_prefix"])
+    ]
     logging.debug(f"Place collections: {place_cols}")
-    
-    logging.info(f"Using GridFS storage at {config['MONGODB_HOST']}")
-    storage = ntpst.NtpStorageGridFs(gridfs_obj=db_lnk.get_gfs(config['documents_col']))
-    backup_storage = ntpst.NtpStorageGridFs(gridfs_obj=db_lnk.get_gfs(config['documents_backup_col']))
+
     files_col = db_lnk.db.get_collection(config['documents_col'] + '.files')
-    
+    buffer = MongoDBBulkWrite(files_col, CTS['UPDATE'], 10000)
+
     if args.drop:
         query = {}
         logging.info("Dropping place_filenames")
         files_col.update_many({}, {'$unset': {'place_filename': 1, 'duplicate' : 1}})
     else:
-        query = {'place_filename': {'$exists': False}}        
-        logging.info("Adding non exisiting place_filenames")
-      
+        query = {'place_filename': {'$exists': False}}
+        logging.info("Adding non existing place_filenames")
 
+    valid_fields  = list(config['STORE_DOC_NAMES'].values())
+    logging.debug(f"Accepted filenames: {valid_fields}")
 
-    for file in files_col.find(query, no_cursor_timeout=True):
-        if not args.drop and 'place_filename' in file and file['place_filename']:
+    processed_docs = set()
+    for file in files_col.find(query, no_cursor_timeout=True, session=mdb_session):
+        if file['filename'] in processed_docs:
             logging.warning(f"File {file['filename']} already processed")
             continue
+
         ntp_id, field = file['filename'].split('_', 1)
-        logging.debug(f"Processing {ntp_id}")   
+
         col = place_cols[nu.get_group(ntp_id)]
+        logging.info(f"Processing {file['filename']}")
         ref_doc = ntp.NtpEntry()
         if not ref_doc.load_from_db(col, ntp_id):
             logging.error(f"Document {ntp_id} not found")
@@ -80,21 +92,52 @@ def main():
         if not ref_doc.data['id']:
             logging.error(f"Document {ntp_id} has no place_id")
             continue
-        place_filename = f"PL{format(int(ref_doc.data['id'].split('/')[-1]), '08d')}_{field}"
+        doc_versions = []
+        field_ok = is_valid_field(valid_fields, field)
+        if field_ok:
+            logging.debug(f"Field {field_ok} is acceptable")
+        else: 
+            logging.debug(f"Field {field} is not acceptable")
         if file['md5']:
-            logging.debug(f"Updating {file['filename']} to {place_filename}")   
-            files_col.update_one({'_id': file['_id']}, {'$set': {'place_filename': place_filename}})
             for duplicate in files_col.find({'md5': file['md5']}):
-                if duplicate['filename'] == file['filename']:
-                    continue    
-                logging.debug(f"Found duplicated document {duplicate['filename']}, marking as duplicate")
-                files_col.update_one(
-                    {'_id': duplicate['_id']}, 
-                    {'$set': {'place_filename': place_filename, 'duplicate':True}}
-                )
+                if field_ok and duplicate['filename'] == file['filename']:
+                    file['duplicate'] = False
+                    continue
+                logging.info(f"Found duplicated document {duplicate['filename']}")
+                dup_id, dup_field = duplicate['filename'].split('_', 1)
+                logging.debug(f"checking {dup_field}")
+                if not field_ok and is_valid_field(valid_fields, dup_field):
+                    field_ok = dup_field
+                    logging.debug(f"Accepted field is {field_ok}")
+                    duplicate['duplicate'] = False
+                    continue
+                duplicate['duplicate']=True
+                doc_versions.append(duplicate)
         else:
             logging.warning(f"File {file['filename']} has no md5")
+            field_ok = field
 
+        if not field_ok:
+            logging.error(f"Valid field name not found for {file['filename']}")
+            continue
+
+        place_filename = f"PL{format(int(ref_doc.data['id'].split('/')[-1]), '08d')}_{field_ok}"
+        logging.info(f"Updating {file['filename']} to {place_filename}")
+        buffer.append(
+            {'_id': file['_id']},
+            {'$set': {'place_filename': place_filename}}
+        )
+        for duplicate in doc_versions:
+            logging.info(f"Updating {duplicate['filename']} to {place_filename}")
+            buffer.append(
+                {'_id': duplicate['_id']},
+                {'$set': {'place_filename': place_filename, 'duplicate': duplicate['duplicate']}}
+            )
+            processed_docs.add(duplicate['filename'])
+        buffer.commit_data_if_full()
+        processed_docs.add(file['filename'])
+
+    buffer.commit_any_data()
 
 
 if __name__ == "__main__":
